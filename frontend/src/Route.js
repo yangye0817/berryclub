b064a3618d376c413def772d8d00a1287b89ba0b
yangye026 <yangye026@ke.com>
2021-03-12 13:53:53 +0800

    引入路由

---
+++ frontend/src/Route.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { HashRouter as Router, Link, Route } from 'react-router-dom';
+import App from './App';
+
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Product = () => (
+  <div>
+    <h2>Product</h2>
+  </div>
+)
+
+class R extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Link to="/">Home</Link>
+          <Link to="/About">About</Link>
+          <Link to="/Product">Product</Link>
+          <hr/>
+          <Route path="/" exact component={App}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route path="/product" component={Product}></Route>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default R;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Route from './Route';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Route />, document.getElementById('root'));
